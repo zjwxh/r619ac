207bc3b31b8a2342f5e249a2ba282b7b100844f6
zjwxh <61620117+zjwxh@users.noreply.github.com>
2020-10-07 12:12:59 +0800

    Update mq1.sh

--- mq1.sh
+++ mq1.sh
@@ -13,4 +13,4 @@
 
 # Add a feed source
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-sed -i 's/KERNEL_PATCHVER:=4.14/KERNEL_PATCHVER:=4.19/g' ./target/linux/ipq40xx/Makefile
+# sed -i 's/KERNEL_PATCHVER:=4.14/KERNEL_PATCHVER:=4.19/g' ./target/linux/ipq40xx/Makefile
